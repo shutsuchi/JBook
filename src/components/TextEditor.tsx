46fb48b6ed23b60173d316a0f7c0b19cb33f9f6d
shutsuchi <wwww.www.wshu@gmail.com>
2021-09-25 15:57:41 +0900

    feat(texteditor.tsx): create markdown-editor component
    using library named react-md-editor, add markdown-editor component and fix some styles

---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,45 @@
+import { FC, useEffect, useRef, useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import './TextEditor.css';
+
+const TextEditor: FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setIsEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (isEditing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+  return (
+    <div className="text-editor card" onClick={() => setIsEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
